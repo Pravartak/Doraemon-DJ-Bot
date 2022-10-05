e4ff1df08216f199cb6c68a1b4515376e13eab65
Pravartak <109897902+Pravartak@users.noreply.github.com>
2022-10-05 16:19:23 +0530

    Create __init__.py

---
+++ Doraemon_DJ/__init__.py
@@ -0,0 +1,41 @@
+from Doraemon_DJ.core.bot import Doraemon_DJBot
+from Doraemon_DJ.core.dir import dirr
+from Doraemon_DJ.core.git import git
+from Doraemon_DJ.core.userbot import Userbot
+from Doraemon_DJ.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = Doraemon_DJBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
