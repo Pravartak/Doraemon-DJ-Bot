683756e3ab7813038c18f7ec992533938212bfc5
Pravartak <109897902+Pravartak@users.noreply.github.com>
2022-10-08 13:42:08 +0530

    Create helpers.py

---
+++ Doraemon_DJ/strings/helpers.py
@@ -0,0 +1,166 @@
+HELP_1 = """**🧑‍✈️<u>ᗩᗪᗰIᑎ ᑕOᗰᗰᗩᑎᗪՏ:</u>**
+
+**ᑕ** ՏTᗩᑎᗪՏ ᖴOᖇ ᑕᕼᗩᑎᑎᗴᒪ ᑭᒪᗩY.
+
+/pause Oᖇ /cpause - ᑭᗩᑌՏᗴ Tᕼᗴ ᑭᒪᗩYIᑎᘜ ᗰᑌՏIᑕ.
+/resume Oᖇ /cresume- ᖇᗴՏᑌᗰᗴ Tᕼᗴ ᑭᗩᑌՏᗴᗪ ᗰᑌՏIᑕ.
+/mute Oᖇ /cmute- ᗰᑌTᗴ Tᕼᗴ ᑭᒪᗩYIᑎᘜ ᗰᑌՏIᑕ.
+/unmute Oᖇ /cunmute- ᑌᑎᗰᑌTᗴ Tᕼᗴ ᗰᑌTᗴᗪ ᗰᑌՏIᑕ.
+/skip Oᖇ /cskip- ՏKIᑭ Tᕼᗴ ᑕᑌᖇᖇᗴᑎT ᑭᒪᗩYIᑎᘜ ᗰᑌՏIᑕ.
+/stop Oᖇ /cstop- ՏTOᑭ Tᕼᗴ ᑭᒪᗩYIᑎᘜ ᗰᑌՏIᑕ.
+/shuffle Oᖇ /cshuffle- ᖇᗩᑎᗪOᗰᒪY ՏᕼᑌᖴᖴᒪᗴՏ Tᕼᗴ ᑫᑌᗴᑌᗴᗪ ᑭᒪᗩYᒪIՏT.
+/seek Oᖇ /cseek - ᖴOᖇᗯᗩᖇᗪ ՏᗴᗴK Tᕼᗴ ᗰᑌՏIᑕ TO YOᑌᖇ ᗪᑌᖇᗩTIOᑎ.
+/seekback Oᖇ /cseekback - ᗷᗩᑕKᗯᗩᖇᗪ ՏᗴᗴK Tᕼᗴ ᗰᑌՏIᑕ TO YOᑌᖇ ᗪᑌᖇᗩTIOᑎ.
+/restart - ᖇᗴՏTᗩᖇT ᗷOT ᖴOᖇ YOᑌᖇ ᑕᕼᗩT.
+
+
+😒<u>**ՏᑭᗴᑕIᖴIᑕ ՏKIᑭ:**</u>
+/skip Oᖇ /cskip [Number(example: 3)] 
+    - ՏKIᑭՏ ᗰᑌՏIᑕ TO Tᕼᗴ ՏᑭᗴᑕIᖴIᗴᗪ ᑫᑌᗴᑌᗴᗪ ᑎᑌᗰᗷᗴᖇ. ᗴ᙭ᗩᗰᑭᒪᗴ: /skip 3 ᗯIᒪᒪ ՏKIᑭ ᗰᑌՏIᑕ TO TᕼIᖇᗪ ᑫᑌᗴᑌᗴᗪ ᗰᑌՏIᑕ ᗩᑎᗪ ᗯIᒪᒪ IᘜᑎOᖇᗴ 1 ᗩᑎᗪ 2 ᗰᑌՏIᑕ Iᑎ ᑫᑌᗴᑌᗴ.
+
+🥱<u>**ᒪOOᑭ ᑭᒪᗩY:**</u>
+/loop Oᖇ /cloop [Enable/Disable] Oᖇ [ᑎᑌᗰᗷᗴᖇՏ ᗷᗴTᗯᗴᗴᑎ 1-10] 
+    - ᗯᕼᗴᑎ ᗩᑕTIᐯᗩTᗴᗪ, ᗷOT ᒪOOᑭՏ Tᕼᗴ ᑕᑌᖇᖇᗴᑎT ᑭᒪᗩYIᑎᘜ ᗰᑌՏIᑕ TO 1-10 TIᗰᗴՏ Oᑎ ᐯOIᑕᗴ ᑕᕼᗩT. ᗪᗴᖴᗩᑌᒪT TO 10 TIᗰᗴՏ.
+
+😎<u>**ᗩᑌTᕼ ᑌՏᗴᖇՏ:**</u>
+ᗩᑌTᕼ ᑌՏᗴᖇՏ ᑕᗩᑎ ᑌՏᗴ ᗩᗪᗰIᑎ ᑕOᗰᗰᗩᑎᗪՏ ᗯITᕼOᑌT ᗩᗪᗰIᑎ ᖇIᘜᕼTՏ Iᑎ YOᑌᖇ ᑕᕼᗩT.
+
+/auth [Username] - ᗩᗪᗪ ᗩ ᑌՏᗴᖇ TO ᗩᑌTᕼ ᒪIՏT Oᖴ Tᕼᗴ ᘜᖇOᑌᑭ.
+/unauth [Username] - ᖇᗴᗰOᐯᗴ ᗩ ᑌՏᗴᖇ ᖴᖇOᗰ ᗩᑌTᕼ ᒪIՏT Oᖴ Tᕼᗴ ᘜᖇOᑌᑭ.
+/authusers - ᑕᕼᗴᑕK ᗩᑌTᕼ ᒪIՏT Oᖴ Tᕼᗴ ᘜᖇOᑌᑭ."""
+
+
+HELP_2 = """▶️<u>**ᑭᒪᗩY ᑕOᗰᗰᗩᑎᗪՏ:**</u>
+
+ᗩᐯᗩIᒪᗩᗷᒪᗴ ᑕOᗰᗰᗩᑎᗪՏ = /play , /vplay , /cplay
+
+ᖴOᖇᑕᗴᑭᒪᗩY ᑕOᗰᗰᗩᑎᗪՏ = /playforce , /vplayforce , /cplayforce
+
+**c** ՏTᗩᑎᗪՏ ᖴOᖇ ᑕᕼᗩᑎᑎᗴᒪ ᑭᒪᗩY.
+**v** ՏTᗩᑎᗪՏ ᖴOᖇ ᐯIᗪᗴO ᑭᒪᗩY.
+**force** ՏTᗩᑎᗪՏ ᖴOᖇ ᖴOᖇᑕᗴ ᑭᒪᗩY.
+
+/play Oᖇ /vplay Oᖇ /cplay  - ᗷOT ᗯIᒪᒪ ՏTᗩᖇT ᑭᒪᗩYIᑎᘜ YOᑌᖇ ᘜIᐯᗴᑎ ᑫᑌᗴᖇY Oᑎ ᐯOIᑕᗴ ᑕᕼᗩT Oᖇ ՏTᖇᗴᗩᗰ ᒪIᐯᗴ ᒪIᑎKՏ Oᑎ ᐯOIᑕᗴ ᑕᕼᗩT.
+
+/playforce Oᖇ /vplayforce Oᖇ /cplayforce -  **ᖴOᖇᑕᗴ ᑭᒪᗩY** ՏTOᑭՏ Tᕼᗴ ᑕᑌᖇᖇᗴᑎT ᑭᒪᗩYIᑎᘜ TᖇᗩᑕK Oᑎ ᐯOIᑕᗴ ᑕᕼᗩT ᗩᑎᗪ ՏTᗩᖇTՏ ᑭᒪᗩYIᑎᘜ Tᕼᗴ Տᗴᗩᖇᑕᕼᗴᗪ TᖇᗩᑕK IᑎՏTᗩᑎTᒪY ᗯITᕼOᑌT ᗪIՏTᑌᖇᗷIᑎᘜ/ᑕᒪᗴᗩᖇIᑎᘜ ᑫᑌᗴᑌᗴ.
+
+/channelplay [Chat Username Or ID] Oᖇ [Disable] - ᑕOᑎᑎᗴᑕT ᑕᕼᗩᑎᑎᗴᒪ TO ᗩ ᘜᖇOᑌᑭ ᗩᑎᗪ ՏTᖇᗴᗩᗰ ᗰᑌՏIᑕ Oᑎ ᑕᕼᗩᑎᑎᗴᒪ'Տ ᐯOIᑕᗴ ᑕᕼᗩT ᖴᖇOᗰ YOᑌᖇ ᘜᖇOᑌᑭ.
+
+
+🤖**<u>ᗷOT'Տ Տᗴᖇᐯᗴᖇ ᑭᒪᗩYᒪIՏTՏ:</u>**
+/playlist  - ᑕᕼᗴᑕK YOᑌᖇ Տᗩᐯᗴᗪ ᑭᒪᗩYᒪIՏT Oᑎ ՏᗴᖇᐯᗴᖇՏ.
+/deleteplaylist - ᗪᗴᒪᗴTᗴ ᗩᑎY Տᗩᐯᗴᗪ ᗰᑌՏIᑕ Iᑎ YOᑌᖇ ᑭᒪᗩYᒪIՏT.
+/play  - ՏTᗩᖇT ᑭᒪᗩYIᑎᘜ YOᑌᖇ Տᗩᐯᗴᗪ ᑭᒪᗩYᒪIՏT ᖴᖇOᗰ ՏᗴᖇᐯᗴᖇՏ."""
+
+
+HELP_3 = """🤖<u>**ᗷOT ᑕOᗰᗰᗩᑎᗪՏ:**</u>
+
+/stats - ᘜᗴT TOᑭ 10 TᖇᗩᑕKՏ ᘜᒪOᗷᗩᒪ ՏTᗩTՏ, TOᑭ 10 ᑌՏᗴᖇՏ Oᖴ ᗷOT, TOᑭ 10 ᑕᕼᗩTՏ Oᑎ ᗷOT, TOᑭ 10 ᑭᒪᗩYᗴᗪ Iᑎ ᗩ ᑕᕼᗩT, ᗷᒪᗩᕼ ᗷᒪᗩᕼ ᗷᒪᗩᕼ...
+
+/sudolist - ᑕᕼᗴᑕK ՏᑌᗪO ᑌՏᗴᖇՏ Oᖴ ᗪOᖇᗩᗴᗰOᑎ ᗪᒍ ᗷOT
+
+/lyrics [Music Name] - ՏᗴᗩᖇᑕᕼᗴՏ ᒪYᖇIᑕՏ ᖴOᖇ Tᕼᗴ ᑭᗩᖇTIᑕᑌᒪᗩᖇ ᗰᑌՏIᑕ Oᑎ ᗯᗴᗷ.
+
+/song [Track Name] Oᖇ [YT Link] - ᗪOᗯᑎᒪOᗩᗪ ᗩᑎY TᖇᗩᑕK ᖴᖇOᗰ YouTube Iᑎ mp3 Oᖇ mp4 ᖴOᖇᗰᗩTՏ.
+
+/player -  ᘜᗴT ᗩ IᑎTᗴᖇᗩᑕTIᐯᗴ ᑭᒪᗩYIᑎᘜ ᑭᗩᑎᗴᒪ.
+
+**c** ՏTᗩᑎᗪՏ ᖴOᖇ ᑕᕼᗩᑎᑎᗴᒪ ᑭᒪᗩY
+
+/queue Oᖇ /cqueue- ᑕᕼᗴᑕK ᑫᑌᗴᑌᗴ ᒪIՏT Oᖴ ᗰᑌՏIᑕ."""
+
+HELP_4 = """💫<u>**ᗴ᙭Tᖇᗩ ᑕOᗰᗰᗩᑎᗪՏ:**</u>
+/start - ՏTᗩᖇT Tᕼᗴ ᗰᑌՏIᑕ ᗷOT. YOᑌ ᕼᗩᐯᗴ ᑭᖇOᗷᗩᗷᒪY ᑌՏᗴᗪ TᕼIՏ😂😅.
+/help  - ᘜᗴT ᑕOᗰᗰᗩᑎᗪՏ ᕼᗴᒪᑭᗴᖇ ᗰᗴᑎᑌ ᗯITᕼ ᗪᗴTᗩIᒪᗴᗪ ᗴ᙭ᑭᒪᗩᑎᗩTIOᑎՏ Oᖴ ᑕOᗰᗰᗩᑎᗪՏ.
+/ping- ᑭIᑎᘜ Tᕼᗴ ᗷOT ᗩᑎᗪ ᑕᕼᗴᑕK ᖇᗩᗰ, ᑕᑭᑌ ᗴTᑕ. ᒍᑌՏT Tᕼᗴ ՏTᗩTՏ Oᖴ ᗷOT.
+
+⚙️<u>**ᘜᖇOᑌᑭ ՏᗴTTIᑎᘜՏ:**</u>
+/settings - ᘜᗴT ᗩ ᑕOᗰᑭᒪᗴTᗴ ᘜᖇOᑌᑭ'Տ ՏᗴTTIᑎᘜՏ ᗯITᕼ IᑎᒪIᑎᗴ ᗷᑌTTOᑎՏ
+
+🔘**OᑭTIOᑎՏ Iᑎ ՏᗴTTIᑎᘜՏ:**
+
+1️⃣ YOᑌ ᑕᗩᑎ ՏᗴT **Audio Quality** YOᑌ ᗯᗩᑎT TO ՏTᖇᗴᗩᗰ Oᑎ ᐯOIᑕᗴ ᑕᕼᗩT.
+
+2️⃣ YOᑌ ᑕᗩᑎ ՏᗴT **Video Quality** YOᑌ ᗯᗩᑎT TO ՏTᖇᗴᗩᗰ Oᑎ ᐯOIᑕᗴ ᑕᕼᗩT.
+
+3️⃣ **ᗩᑌTᕼ ᑌՏᗴᖇՏ**:- YOᑌ ᑕᗩᑎ ᑕᕼᗩᑎᘜᗴ ᗩᗪᗰIᑎ ᑕOᗰᗰᗩᑎᗪՏ ᗰOᗪᗴ ᖴᖇOᗰ ᕼᗴᖇᗴ TO ᗴᐯᗴᖇYOᑎᗴ Oᖇ ᗩᗪᗰIᑎՏ OᑎᒪY. Iᖴ ᗴᐯᗴᖇYOᑎᗴ, ᗩᑎYOᑎᗴ ᑭᖇᗴՏᗴᑎT Iᑎ YOᑌᖇ ᘜᖇOᑌᑭ ᗯIᒪᒪ ᗷᗴ ᗩᗷᒪᗴ TO ᑌՏᗴ ᗩᗪᗰIᑎ ᑕOᗰᗰᗩᑎᗪՏ ᒪIKᗴ /skip, /stop, ᗴTᑕ.
+
+4️⃣ **ᑕᒪᗴᗩᑎ ᗰOᗪᗴ:** ᗯᕼᗴᑎ ᗴᑎᗩᗷᒪᗴᗪ ᗪᗴᒪᗴTᗴՏ Tᕼᗴ ᗷOT'Տ ᗰᗴՏՏᗩᘜᗴՏ ᗩᖴTᗴᖇ 5 ᗰIᑎՏ ᖴᖇOᗰ YOᑌᖇ ᘜᖇOᑌᑭ TO ᗰᗩKᗴ Տᑌᖇᗴ YOᑌᖇ ᑕᕼᗩT ᖇᗴᗰᗩIᑎՏ ᑕᒪᗴᗩᑎ ᗩᑎᗪ ᘜOOᗪ.
+
+5️⃣ **ᑕOᗰᗰᗩᑎᗪ ᑕᒪᗴᗩᑎ** : ᗯᕼᗴᑎ ᗩᑕTIᐯᗩTᗴᗪ, ᗷOT ᗯIᒪᒪ ᗪᗴᒪᗴTᗴ ITՏ ᗴ᙭ᗴᑕᑌTᗴᗪ ᑕOᗰᗰᗩᑎᗪՏ (/play, /pause, /shuffle, /stop ᗴTᑕ) IᗰᗰᗴᗪIᗩTᗴᒪY.
+
+6️⃣ **ᑭᒪᗩY ՏᗴTTIᑎᘜՏ:**
+
+/playmode - ᘜᗴT ᗩ ᑕOᗰᑭᒪᗴTᗴ ᑭᒪᗩY ՏᗴTTIᑎᘜՏ ᑭᗩᑎᗴᒪ ᗯITᕼ ᗷᑌTTOᑎՏ ᗯᕼᗴᖇᗴ YOᑌ ᑕᗩᑎ ՏᗴT YOᑌᖇ ᘜᖇOᑌᑭ'Տ ᑭᒪᗩY ՏᗴTTIᑎᘜՏ. 
+
+<u>OᑭTIOᑎՏ Iᑎ ᑭᒪᗩYᗰOᗪᗴ:</u>
+
+1️⃣ **Տᗴᗩᖇᑕᕼ ᗰOᗪᗴ** [Direct or Inline] - ᑕᕼᗩᑎᘜᗴՏ YOᑌᖇ Տᗴᗩᖇᑕᕼ ᗰOᗪᗴ ᗯᕼIᒪᗴ YOᑌ ᘜIᐯᗴ /play mode. 
+
+2️⃣ **ᗩᗪᗰIᑎ ᑕOᗰᗰᗩᑎᗪՏ** [Everyone or Admins] - Iᖴ ᗴᐯᗴᖇYOᑎᗴ, ᗩᑎYOᑎᗴ ᑭᖇᗴՏᗴᑎT Iᑎ YOᑌᖇ ᘜᖇOᑌᑭ ᗯIᒪᒪ ᗷᗴ ᗩᗷᒪᗴ TO ᑌՏᗴ ᗩᗪᗰIᑎ ᑕOᗰᗰᗩᑎᗪՏ ᒪIKᗴ /skip, /stop, ᗴTᑕ.
+
+3️⃣ **ᑭᒪᗩY TYᑭᗴ** [Everyone or Admins] - Iᖴ ᗩᗪᗰIᑎՏ, OᑎᒪY ᗩᗪᗰIᑎՏ ᑭᖇᗴՏᗴᑎT Iᑎ ᘜᖇOᑌᑭ ᑕᗩᑎ ᑭᒪᗩY ᗰᑌՏIᑕ Oᑎ ᐯOIᑕᗴ ᑕᕼᗩT."""
+
+HELP_5 = """🔰**<u>ᗩᗪᗪ/ᖇᗴᗰOᐯᗴ ՏᑌᗪO ᑌՏᗴᖇՏ:</u>**
+/addsudo [Username or Reply to a user]
+/delsudo [Username or Reply to a user]
+
+🛃**<u>ᕼᗴᖇOKᑌ:</u>**
+/usage - ՏᕼOᗯՏ ᗪYᑎO ᑌՏᗩᘜᗴ.
+
+🌐**<u>ᑕOᑎᖴIᘜ ᐯᗩᖇՏ:</u>**
+/get_var - ᘜᗴT ᗩ ᑕOᑎᖴIᘜ ᐯᗩᖇ ᖴᖇOᗰ ᕼᗴᖇOKᑌ Oᖇ .env.
+/del_var - ᗪᗴᒪᗴTᗴ ᗩᑎY ᐯᗩᖇ Oᑎ ᕼᗴᖇOKᑌ Oᖇ .env.
+/set_var [Var Name] [Value] - ՏᗴT ᗩ ᐯᗩᖇ Oᖇ ᑌᑭᗪᗩTᗴ ᗩ ᐯᗩᖇ Oᑎ ᕼᗴᖇOKᑌ Oᖇ .env. ՏᗴᑭᗩᖇᗩTᗴ ᐯᗩᖇ ᗩᑎᗪ IT'Տ ᐯᗩᒪᑌᗴ ᗯITᕼ ᗩ Տᑭᗩᑕᗴ.
+
+🤖**<u>ᗷOT ᑕOᗰᗰᗩᑎᗪՏ:</u>**
+/reboot - ᖇᗴᗷOOT YOᑌᖇ ᗷOT.
+/update - ᑌᑭᗪᗩTᗴ ᗷOT.
+/speedtest - ᑕᕼᗴᑕK Տᗴᖇᐯᗴᖇ Տᑭᗴᗴᗪ.
+/maintenance [enable / disable] 
+/logger [enable / disable] - ᗷOT ᒪOᘜՏ Tᕼᗴ Տᗴᗩᖇᑕᕼᗴᗪ ᑫᑌᗴᖇIᗴՏ Iᑎ ᒪOᘜᘜᗴᖇ ᘜᖇOᑌᑭ.
+/get_log [Number of Lines] - ᘜᗴT ᒪOᘜ Oᖴ YOᑌᖇ ᗷOT ᖴᖇOᗰ ᕼᗴᖇOKᑌ Oᖇ ᐯᑭՏ. ᗯOᖇKՏ ᖴOᖇ ᗷOTᕼ.
+/autoend [enable|disable] - ᗴᑎᗩᗷᒪᗴ ᗩᑌTO ՏTᖇᗴᗩᗰ ᗴᑎᗪ ᗩᖴTᗴᖇ 3 ᗰIᑎՏ Iᖴ ᑎO Oᑎᗴ IՏ ᒪIՏTᗴᑎIᑎᘜ.
+
+📈**<u>ՏTᗩTՏ ᑕOᗰᗰᗩᑎᗪՏ:</u>**
+/activevoice - ᑕᕼᗴᑕK ᗩᑕTIᐯᗴ ᐯOIᑕᗴ ᑕᕼᗩTՏ Oᑎ ᗷOT.
+/activevideo - ᑕᕼᗴᑕK ᗩᑕTIᐯᗴ ᐯIᗪᗴO ᑕᗩᒪᒪՏ Oᑎ ᗷOT.
+/stats - ᑕᕼᗴᑕK ᗷOT ՏTᗩTՏ
+
+⚠️**<u>ᗷᒪᗩᑕKᒪIՏT ᑕᕼᗩT ᖴᑌᑎᑕTIOᑎ:</u>**
+/blacklistchat [CHAT_ID] - ᗷᒪᗩᑕKᒪIՏT ᗩᑎY ᑕᕼᗩT ᖴᖇOᗰ ᑌՏIᑎᘜ ᗰᑌՏIᑕ ᗷOT.
+/whitelistchat [CHAT_ID] - ᗯᕼITᗴᒪIՏT ᗩᑎY ᗷᒪᗩᑕKᒪIՏTᗴᗪ ᑕᕼᗩT ᖴᖇOᗰ ᑌՏIᑎᘜ ᗰᑌՏIᑕ ᗷOT.
+/blacklistedchat - ᑕᕼᗴᑕK ᗩᒪᒪ ᗷᒪᗩᑕKᒪIՏTᗴᗪ ᑕᕼᗩTՏ.
+
+👤**<u>ᗷᒪOᑕKᗴᗪ ᖴᑌᑎᑕTIOᑎ:</u>**
+/block [Username or Reply to a user] - ᑭᖇᗴᐯᗴᑎTՏ ᗩ ᑌՏᗴᖇ ᖴᖇOᗰ ᑌՏIᑎᘜ ᗷOT ᑕOᗰᗰᗩᑎᗪՏ.
+/unblock [Username or Reply to a user] - ᖇᗴᗰOᐯᗴ ᗩ ᑌՏᗴᖇ ᖴᖇOᗰ ᗷOT'Տ ᗷᒪOᑕKᗴᗪ ᒪIՏT.
+/blockedusers - ᑕᕼᗴᑕK ᗷᒪOᑕKᗴᗪ ᑌՏᗴᖇՏ ᒪIՏT
+
+👤**<u>ᘜᗷᗩᑎ ᖴᑌᑎᑕTIOᑎ:</u>**
+/gban [Username or Reply to a user] - ᘜᗷᗩᑎ ᗩ ᑌՏᗴᖇ ᖴᖇOᗰ ᗷOT'Տ Տᗴᖇᐯᗴᗪ ᑕᕼᗩT ᗩᑎᗪ ՏTOᑭ ᕼIᗰ ᖴᖇOᗰ ᑌՏIᑎᘜ YOᑌᖇ ᗷOT.
+/ungban [Username or Reply to a user] - ᖇᗴᗰOᐯᗴ ᗩ ᑌՏᗴᖇ ᖴᖇOᗰ ᗷOT'Տ ᘜᗷᗩᑎᑎᗴᗪ ᒪIՏT ᗩᑎᗪ ᗩᒪᒪOᗯ ᕼIᗰ ᖴOᖇ ᑌՏIᑎᘜ YOᑌᖇ ᗷOT.
+/gbannedusers - ᑕᕼᗴᑕK ᘜᗷᗩᑎᑎᗴᗪ ᑌՏᗴᖇՏ ᒪIՏT.
+
+🎥**<u>ᐯIᗪᗴOᑕᗩᒪᒪՏ ᖴᑌᑎᑕTIOᑎ:</u>**
+/set_video_limit [Number of Chats] - ՏᗴT ᗩ ᗰᗩ᙭Iᗰᑌᗰ ᑎᑌᗰᗷᗴᖇ Oᖴ ᑕᕼᗩTՏ ᗩᒪᒪOᗯᗴᗪ ᖴOᖇ ᐯIᗪᗴO ᑕᗩᒪᒪՏ ᗩT ᗩ TIᗰᗴ. ᗪᗴᖴᗩᑌᒪT TO 3 ᑕᕼᗩTՏ.
+/videomode [download|m3u8] - Iᖴ ᗪOᗯᑎᒪOᗩᗪ ᗰOᗪᗴ IՏ ᗴᑎᗩᗷᒪᗴᗪ, ᗷOT ᗯIᒪᒪ ᗪOᗯᑎᒪOᗩᗪ ᐯIᗪᗴOՏ IᑎՏTᗴᗩᗪ Oᖴ ᑭᒪᗩYIᑎᘜ Tᕼᗴᗰ Iᑎ M3u8 ᖴOᖇᗰ. ᗷYᗪᗴᖴᗩᑌᒪT TO M3u8 ᖴOᖇᗰ. YOᑌ ᑕᗩᑎ ᑌՏᗴ ᗪOᗯᑎᒪOᗩᗪ ᗰOᗪᗴ ᗯᕼᗴᑎ ᗩᑎY ᑫᑌᗴᖇY ᗪOᗴՏᑎ'T ᑭᒪᗩY Iᑎ M3U8 ᗰOᗪᗴ.
+
+⚡️**<u>ᑭᖇIᐯᗩTᗴ ᗷOT ᖴᑌᑎᑕTIOᑎ:</u>**
+/authorize [CHAT_ID] - ᗩᒪᒪOᗯ ᗩ ᑕᕼᗩT ᖴOᖇ ᑌՏIᑎᘜ YOᑌᖇ ᗷOT.
+/unauthorize [CHAT_ID] - ᗪIՏᗩᒪᒪOᗯ ᗩ ᑕᕼᗩT ᖴOᖇ ᑌՏIᑎᘜ YOᑌᖇ ᗷOT.
+/authorized - ᑕᕼᗴᑕK ᗩᒪᒪ ᗩᒪᒪOᗯᗴᗪ ᑕᕼᗩTՏ Oᖴ YOᑌᖇ ᗷOT.
+
+🌐**<u>ᗷᖇOᗩᗪᑕᗩՏT ᖴᑌᑎᑕTIOᑎ:</u>**
+/broadcast [Message or Reply to a Message] - ᗷᖇOᗩᗪᑕᗩՏT ᗩᑎY ᗰᗴՏՏᗩᘜᗴ TO ᗷOT'Տ Տᗴᖇᐯᗴᗪ ᑕᕼᗩTՏ.
+
+<u>OᑭTIOᑎՏ ᖴOᖇ ᗷᖇOᗩᗪᑕᗩՏT:</u>
+**-pin** : TᕼIՏ ᗯIᒪᒪ ᑭIᑎ YOᑌᖇ ᗰᗴՏՏᗩᘜᗴ.
+**-pinloud** : TᕼIՏ ᗯIᒪᒪ ᑭIᑎ YOᑌᖇ ᗰᗴՏՏᗩᘜᗴ ᗯITᕼ ᒪOᑌᗪ ᑎOTIᖴIᑕᗩTIOᑎ.
+**-user** : TᕼIՏ ᗯIᒪᒪ ᗷᖇOᗩᗪᑕᗩՏT YOᑌᖇ ᗰᗴՏՏᗩᘜᗴ TO Tᕼᗴ ᑌՏᗴᖇՏ ᗯᕼO ᕼᗩᐯᗴ ՏTᗩᖇTᗴᗪ YOᑌᖇ ᗷOT.
+**-assistant** : TᕼIՏ ᗯIᒪᒪ ᗷᖇOᗩᗪᑕᗩՏT YOᑌᖇ ᗰᗴՏՏᗩᘜᗴ ᖴᖇOᗰ ᗩՏՏIՏTᗩᑎT ᗩᑕᑕOᑌᑎT Oᖴ YOᑌᖇ ᗷOT.
+**-nobot** : TᕼIՏ ᗯIᒪᒪ ᖴOᖇᑕᗴ YOᑌᖇ ᗷOT TO ᑎOT ᗷᖇOᗩᗪᑕᗩՏT ᗰᗴՏՏᗩᘜᗴ.
+
+**ᗴ᙭ᗩᗰᑭᒪᗴ:** `/broadcast -user -assistant -pin ᕼᗴᒪᒪO TᗴՏTIᑎᘜ ᗷᖇOᗩᗪᑕᗩՏT´
+
+"""
