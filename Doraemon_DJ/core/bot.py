e547ad334462b79dc2c18bf5a0e68e527b98a154
Pravartak <109897902+Pravartak@users.noreply.github.com>
2022-10-08 13:10:35 +0530

    Create bot.py

---
+++ Doraemon_DJ/core/bot.py
@@ -0,0 +1,53 @@
+import sys
+
+from pyrogram import Client
+
+import config
+
+from ..logging import LOGGER
+
+
+class DoraemonBot(Client):
+    def __init__(self):
+        LOGGER(__name__).info(f"Starting Bot... ")
+        super().__init__(
+            "DoraemonBot",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+        )
+
+    async def start(self):
+        await super().start()
+        get_me = await self.get_me()
+        self.username = get_me.username
+        self.id = get_me.id
+        try:
+            await self.send_message(
+                config.LOG_GROUP_ID, "Bot Started"
+            )
+        except:
+            LOGGER(__name__).error(
+                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+            )
+            sys.exit()
+        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+        if a.status != "administrator":
+            LOGGER(__name__).error(
+                "Please promote Bot as Admin in Logger Group"
+            )
+            sys.exit()
+        if get_me.last_name:
+            self.name = get_me.first_name + " " + get_me.last_name
+        else:
+            self.name = get_me.first_name
+        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+try:
+            await self.send_message(
+                config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᗷOT ՏTᗩᖇTᗴᗪ:**\n\n✨ Iᗪ: `{self.id}`\n❄ ᑎᗩᗰᗴ: {self.name}\n💫 ᑌՏᗴᖇᑎᗩᗰᗴ: @{self.username}"
+            )
+        except:
+            LOGGER(__name__).error(
+                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+            )
+            sys.exit()
